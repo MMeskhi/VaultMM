5d00732690a1e8b5e9d40115323188d9df03fac3
MMeskhi <mishameskhi1@gmail.com>
2026-09-20 00:16:30 +0400

    refactor

--- server/VaultMM/VaultMM.Server/obj/Release/net10.0/VaultMM.Server.AssemblyInfo.cs
+++ server/VaultMM/VaultMM.Server/obj/Release/net10.0/VaultMM.Server.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("VaultMM.Server")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6fdff31f0b130ea01a3dc523544a7ce1eaf1aec")]
 [assembly: System.Reflection.AssemblyProductAttribute("VaultMM.Server")]
