b8801c0c6d6fc24618d1c969a176710ba800f29f
MMeskhi <mishameskhi1@gmail.com>
2026-09-19 16:09:47 +0400

    feat server project

---
+++ server/VaultMM/VaultMM.Server/Controllers/VaultItemController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VaultMM.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VaultItemController : ControllerBase
+    {
+        [HttpGet]
+        public VaultItem Get()
+        {
+            return new VaultItem
+            {
+                Id = 1,
+                Title = "Blade Runner 2049",
+                Description = "Sci-fi movie",
+                Url = "https://www.letterboxd.com/",
+                ImageUrl = null,
+                VaultId = 1
+            };
+        }
+    }
+}
---
+++ server/VaultMM/VaultMM.Server/VaultItem.cs
@@ -0,0 +1,20 @@
+﻿namespace VaultMM.Server
+{
+    public class VaultItem
+    {
+        public int Id { get; set; }
+
+        public required string Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? Url { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        public int VaultId { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
